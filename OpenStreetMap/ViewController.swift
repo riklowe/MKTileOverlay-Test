ea6073cfd8f52bfe76d02630e94a49228854d160
Richard Lowe <riklowe@Richards-MacBook-Air.local>
2016-11-24 14:49:35 +0000

    simplified code and added new maps

--- OpenStreetMap/ViewController.swift
+++ OpenStreetMap/ViewController.swift
@@ -13,125 +13,163 @@ import MapKit
 //var template = "http://tile.stamen.com/watercolor/{z}/{x}/{y}.png"
 //var overlay =  MKTileOverlay (urlTemplate: template)
 
+
+/*
+
+ http://wiki.openstreetmap.org/wiki/Tile_servers
+ 
+ https://community.tibco.com/wiki/geoanalytics-resources
+ 
+
+*/
+
 class ViewController: UIViewController, MKMapViewDelegate {
 
 
     @IBOutlet weak var MapView: MKMapView!
+
+    
+    @IBAction func snow(_ sender: UIBarButtonItem) {
+
+        //let template = "http://www.opensnowmap.org/opensnowmap-overlay/{z}/{x}/{y}.png"
+        //let template = "http://tile1.maptoolkit.net/bikemap/{z}/{x}/{y}.png"
+
+        let template = "http://tile1.maptoolkit.net/terrain/{z}/{x}/{y}.png"
+        showMap(temp: template, olay: true, clear: true, tileSize: 256)
+        
+    }
     
     @IBAction func terrain(_ sender: Any) {
-        let overlays = MapView.overlays
-        MapView.removeOverlays(overlays)
         
         let template = "http://tile.stamen.com/terrain/{z}/{x}/{y}.png"
-        
-        let overlay = MKTileOverlay (urlTemplate: template)
-        overlay.canReplaceMapContent = true
-        
-        MapView.add(overlay, level: .aboveLabels)
-        
-        MapView.setNeedsDisplay()
+        showMap(temp: template, olay: true, clear: true, tileSize: 256)
+
     }
     
     @IBAction func cycle(_ sender: Any) {
-        let overlays = MapView.overlays
-        MapView.removeOverlays(overlays)
         
         let template = "http://tile.opencyclemap.org/cycle/{z}/{x}/{y}.png"
-        
-        let overlay = MKTileOverlay (urlTemplate: template)
-        overlay.canReplaceMapContent = true
-        
-        MapView.add(overlay, level: .aboveLabels)
-        
-        MapView.setNeedsDisplay()
+        showMap(temp: template, olay: true, clear: true, tileSize: 256)
 
     }
     @IBAction func toner(_ sender: Any) {
-        let overlays = MapView.overlays
-        MapView.removeOverlays(overlays)
-        
-        let template = "http://tile.stamen.com/toner/{z}/{x}/{y}.png"
-        
-        let overlay = MKTileOverlay (urlTemplate: template)
-        overlay.canReplaceMapContent = true
-        
-        MapView.add(overlay, level: .aboveLabels)
-        
-        MapView.setNeedsDisplay()
+
+        let template = "http://a.tile.stamen.com/toner/{z}/{x}/{y}.png"
+        showMap(temp: template, olay: true, clear: true, tileSize: 256)
 
     }
     
     @IBAction func xport(_ sender: UIBarButtonItem) {
-        let overlays = MapView.overlays
-        MapView.removeOverlays(overlays)
-        
         let template = "http://tile.opencyclemap.org/transport/{z}/{x}/{y}.png"
-        
-        let overlay = MKTileOverlay (urlTemplate: template)
-        overlay.canReplaceMapContent = true
-        
-        MapView.add(overlay, level: .aboveLabels)
-        
-        MapView.setNeedsDisplay()
-    }
-
-    @IBAction func train(_ sender: UIBarButtonItem) {
-        let overlays = MapView.overlays
-        MapView.removeOverlays(overlays)
-        
-        let template = "http://tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png"
-        //let template = "http://tiles.openrailwaymap.org/standard/${z}/${x}/${y}.png"
-        
-        let overlay = MKTileOverlay (urlTemplate: template)
-        overlay.canReplaceMapContent = true
-        
-        MapView.add(overlay, level: .aboveLabels)
-        
-        MapView.setNeedsDisplay()
+        showMap(temp: template, olay: true, clear: true, tileSize: 256)
     }
     
     @IBAction func choseMap(_ sender: UIBarButtonItem) {
-        
-        let overlays = MapView.overlays
-        MapView.removeOverlays(overlays)
-        
         let template = "http://tile.stamen.com/watercolor/{z}/{x}/{y}.png"
+        showMap(temp: template, olay: true, clear: true, tileSize: 256)
+    }
+    
+    @IBAction func thunder(_ sender: UIBarButtonItem) {
 
-        let overlay = MKTileOverlay (urlTemplate: template)
-        overlay.canReplaceMapContent = true
+        //let template = "http://tile.thunderforest.com/landscape/{z}/{x}/{y}.png"
+        //let template = "http://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png"
+        //let template = "http://tile.thunderforest.com/spinal-map/{z}/{x}/{y}.png"
+        let template = "http://tile.thunderforest.com/pioneer/{z}/{x}/{y}.png"
+        //let template = "http://tile.thunderforest.com/transport/{z}/{x}/{y}.png"
         
-        MapView.add(overlay, level: .aboveLabels)
+        //let template = "http://tile.thunderforest.com/cycle/{z}/{x}/{y}.png"
+        showMap(temp: template, olay: true, clear: true, tileSize: 256)
 
-        MapView.setNeedsDisplay()
+    }
+    
+    @IBAction func train(_ sender: UIBarButtonItem) {
+
+        let template = "http://tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png"
+        //let template = "http://tiles.openrailwaymap.org/maxspeed/{z}/{x}/{y}.png"
+        //let template = "http://tiles.openrailwaymap.org/signals/{z}/{x}/{y}.png"
         
+        showMap(temp: template, olay: false, clear: true, tileSize: 512)
+        
+    }
+    
+    @IBAction func topo(_ sender: UIBarButtonItem) {
+        //let template = "http://tile.opentopomap.org/{z}/{x}/{y}.png"
+        //let template = "http://tile.thunderforest.com/spinal-map/{z}/{x}/{y}.png"
+        let template = "http://www.openptmap.org/tiles/{z}/{x}/{y}.png"
+        showMap(temp: template, olay: false, clear: true, tileSize: 256)
+        
+    }
+    
+    @IBAction func hike(_ sender: UIBarButtonItem) {
+        let template = "http://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png"
+        showMap(temp: template, olay: true, clear: true, tileSize: 256)
+    }
+    
+    @IBAction func weather(_ sender: UIBarButtonItem) {
+        var template = "http://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        showMap(temp: template, olay: true, clear: true, tileSize: 256)
+
+        //let template = "http://tile.stamen.com/toner/{z}/{x}/{y}.png"
+        
+        //format
+        //http://weather.openportguide.de/tiles/actual/variable/timestep/zoom/X/Y.png
+        
+        //        timestep 5 ≈ actual time at the moment of the release
+        //        timestep 7 ≈ 2*3h = 6h in the future
+        //        timestep 9 ≈ 4*3h = 12h in the future
+        //        timestep 11 ≈ 6*3h = 18h in the future
+        //        timestep 15 ≈ 10*3h = 30h in the future
+        //        timestep 19 ≈ 14*3h = 42h in the future
+        //        timestep 23 ≈ 18*3h = 54h in the future
+        //        timestep 27 ≈ 22*3h = 66h in the future
+        
+        //rain
+        //http://weather.openportguide.de/tiles/actual/precipitation/5/5/15/10.png
+        //http://weather.openportguide.de/tiles/actual/precipitation_shaded/5/5/15/10.png
+        
+        //temp
+        // http://weather.openportguide.de/tiles/actual/air_temperature/5/5/15/10.png
+        
+        //wave
+        // http://weather.openportguide.de/tiles/actual/significant_wave_height/5/5/15/10.png
+        
+        //Wind
+        // http://weather.openportguide.de/tiles/actual/wind_stream/5/5/15/10.png
+        // http://weather.openportguide.de/tiles/actual/gust/5/5/15/10.png
+        //http://weather.openportguide.de/tiles/actual/FL100_wind_barb/5/5/15/10.png
+        //http://weather.openportguide.de/tiles/actual/FL200_wind_barb/5/5/15/10.png
+        //http://weather.openportguide.de/tiles/actual/FL300_wind_barb/5/5/15/10.png
+        // http://weather.openportguide.de/tiles/actual/FL400_wind_barb/5/5/15/10.png
+        
+        
+        //template = "http://weather.openportguide.de/tiles/actual/surface_pressure/5/{z}/{x}/{y}.png"
+        
+        //template = "http://tile.openweathermap.org/clouds/{z}/{x}/{y}.png"
+        
+        
+        //let template = "http://weather.openportguide.de/tiles/actual/air_temperature/5/{z}/{x}/{y}.png"
+        template = "http://weather.openportguide.de/tiles/actual/wind_stream/5/{z}/{x}/{y}.png"
+        //let template = "http://weather.openportguide.de/tiles/actual/precipitation_shaded/5/{z}/{x}/{y}.png"
+        
+        showMap(temp: template, olay: true, clear: false, tileSize: 256)
+        
+        
+        template = "http://tiles.openseamap.org/seamark/{z}/{x}/{y}.png"
+        showMap(temp: template, olay: true, clear: false, tileSize: 256)
+
     }
     
     @IBAction func OSM(_ sender: UIBarButtonItem) {
         
-        let overlays = MapView.overlays
-        MapView.removeOverlays(overlays)
-
         let template = "http://tile.openstreetmap.org/{z}/{x}/{y}.png"
-
-        let overlay = MKTileOverlay (urlTemplate: template)
-        overlay.canReplaceMapContent = true
-        
-        MapView.add(overlay, level: .aboveLabels)
+        showMap(temp: template, olay: true, clear: true, tileSize: 256)
 
     }
 
     @IBAction func Apple(_ sender: UIBarButtonItem) {
+        
         let overlays = MapView.overlays
         MapView.removeOverlays(overlays)
-        
-        //        let template = "http://tile.opencyclemap.org/transport/{z}/{x}/{y}.png"
-        //
-        //        let overlay = MKTileOverlay (urlTemplate: template)
-        //        overlay.canReplaceMapContent = true
-        //
-        //        MapView.add(overlay, level: .aboveLabels)
-        
-        //MapView.mapType = MKMapType.satelliteFlyover
         
         MapView.showsTraffic = true
         
@@ -140,6 +178,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
     }
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
@@ -154,9 +193,6 @@ class ViewController: UIViewController, MKMapViewDelegate {
         //let template = "http://tile.stamen.com/toner/{z}/{x}/{y}.png"
         //let template = "http://tile.stamen.com/terrain/{z}/{x}/{y}.png"
         //let template = "http://tile.stamen.com/watercolor/{z}/{x}/{y}.png"
-        //let template = "http://tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png"
-        //let template = "http://tiles.openrailwaymap.org/maxspeed/{z}/{x}/{y}.png"
-        //let template = "http://tiles.openrailwaymap.org/signals/{z}/{x}/{y}.png"
         
         
         //CENTER MAP TO THIS POI
@@ -184,13 +220,29 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         MapView.delegate = self
 
-        let overlay = MKTileOverlay (urlTemplate: template)
-        overlay.canReplaceMapContent = true
+        showMap(temp: template, olay: true, clear: false, tileSize: 256)
+        
+    }
+    
+    func showMap(temp : String, olay : Bool, clear : Bool, tileSize : Int) {
+        
+        //clear existing overlays
+        if clear {
+            let overlays = MapView.overlays
+            MapView.removeOverlays(overlays)
+        }
+        
+        let overlay = MKTileOverlay (urlTemplate: temp)
+        overlay.canReplaceMapContent = olay
+        
+        if tileSize > 0 {
+            overlay.tileSize = CGSize(width: tileSize, height: tileSize)
+        }
+        
+        
         
         MapView.add(overlay, level: .aboveLabels)
-        
-        
-        
+
     }
 
     //delegate
--- OpenStreetMap/customOverlay.swift
+++ OpenStreetMap/customOverlay.swift
@@ -6,34 +6,153 @@
 //  Copyright © 2016 Richard Lowe. All rights reserved.
 //
 
-//import Foundation
-//import UIKit
-//import MapKit
-//
-//class MKHipsterTileOverlay : MKTileOverlay {
-//    
-//    let cache = NSCache<AnyObject, AnyObject>()
-//    let operationQueue = OperationQueue()
-//    
-//    func URLForTilePath(path: MKTileOverlayPath) -> NSURL {
-//        return NSURL(string: String(format: "http://tile.openstreetmap.com/%d/%d/%d", path.z, path.x, path.y))!
-//    }
-//    
-//    func loadTileAtPath(path: MKTileOverlayPath, result: @escaping (NSData?, NSError?) -> Void) {
-//        
-//        let url = URLForTilePath(path: path)
-//        if let cachedData = cache.object(forKey: url) as? NSData {
-//            result(cachedData, nil)
-//        } else {
-//            let request = NSURLRequest(url: url as URL)
-//            NSURLConnection.sendAsynchronousRequest(request as URLRequest, queue: operationQueue) {
-//                [weak self]
-//                response, data, error in
-//                if let data = data {
-//                    self?.cache.setObject(data as AnyObject, forKey: url)
-//                }
-//                result(data as NSData?, error as NSError?)
-//            }
-//        }
-//    }
-//}
+import Foundation
+import UIKit
+import MapKit
+
+class customOverlay : MKTileOverlay {
+    
+    let parentDirectory = "tilecache"
+    let maximumCacheAge: TimeInterval = 30.0 * 24.0 * 60.0 * 60.0
+    var urlSession: URLSession?
+    
+    init() {
+        
+        print ("/{z}/{x}/{y}")
+        super.init(urlTemplate: "http://tiles.openseamap.org/seamark/{z}/{x}/{y}.png")
+        //super.init(urlTemplate: "http://tile.openstreetmap.org/{z}/{x}/{y}.png")
+        
+        
+        /*============================================================*/
+        /*============================================================*/
+        /*============================================================*/
+        print ("***** \(#function) *****")
+        
+        
+        self.minimumZ = 9
+        self.maximumZ = 17
+        self.canReplaceMapContent = false
+        
+        // The Open Sea Map tile server returns 404 for blank tiles, and also when it's
+        // too heavily loaded to return a tile. We'll do our own cacheing and not use
+        // NSURLSession's.
+        let sessionConfiguration = URLSessionConfiguration.default
+        sessionConfiguration.urlCache = nil
+        sessionConfiguration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        self.urlSession = URLSession(configuration: sessionConfiguration)
+        
+        //Detect if app is being built for device or simulator in Swift
+        #if (arch(i386) || arch(x86_64)) && os(iOS)
+            let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.cachesDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+            let cachesDirectory = paths[0]
+            print("Caches Directory:")
+            print(cachesDirectory)
+        #endif
+        
+    }
+    
+    //override func loadTileAtPath(path: MKTileOverlayPath, result: ((NSData?, NSError?) -> Void)) {
+    
+    override func loadTile(at path: MKTileOverlayPath, result: @escaping (Data?, Error?) -> Void) {
+        
+        //override func loadTile(at: MKTileOverlayPath, result: @escaping (Data?, Error?) -> Void)
+        
+        /*============================================================*/
+        /*============================================================*/
+        /*============================================================*/
+        print ("***** \(#function) *****")
+        
+        
+        let parentXFolderURL = URLForTilecacheFolder().appendingPathComponent(self.cacheXFolderNameForPath(path: path))
+        let tileFilePathURL = parentXFolderURL.appendingPathComponent(fileNameForTile(path: path))
+        let tileFilePath = tileFilePathURL.path
+        var useCachedVersion = false
+        
+        if FileManager.default.fileExists(atPath: tileFilePath) {
+            if let fileAttributes = try? FileManager.default.attributesOfItem(atPath: tileFilePath),
+                let fileModificationDate = fileAttributes[FileAttributeKey.modificationDate] as? NSDate {
+                if fileModificationDate.timeIntervalSinceNow > -1.0 * maximumCacheAge {
+                    useCachedVersion = true
+                }
+            }
+        }
+        
+        if (useCachedVersion) {
+            let cachedData = NSData(contentsOfFile: tileFilePath)
+            result(cachedData as Data?, nil)
+        }
+        else {
+            let request = URLRequest(url: self.url(forTilePath: path))
+            //				print("fetching", request)
+            
+            
+            let task = urlSession!.dataTask(with: request as URLRequest, completionHandler: { (data, response, error)  in
+                
+                if let data = data {
+                    do {
+                        if response != nil {
+                            if let httpResponse = response as? HTTPURLResponse {
+                                if httpResponse.statusCode == 200 {
+                                    do {
+                                        try FileManager.default.createDirectory(at: parentXFolderURL, withIntermediateDirectories: true, attributes: nil)
+                                    } catch {
+                                    }
+                                    //data.write(to: tileFilePathURL, options: true) {
+                                    
+                                    try data.write(to: tileFilePathURL, options:.atomicWrite  )
+                                    result(data as Data?, error as Error?)
+                                }
+                            }
+                        }
+                    } catch {
+                        
+                    }
+                }
+                
+            })
+            
+            task.resume()
+        }
+    }
+    
+    // filename for y.png, used within the cacheXFolderNameForPath
+    private func fileNameForTile(path: MKTileOverlayPath) -> String {
+        /*============================================================*/
+        /*============================================================*/
+        /*============================================================*/
+        print ("***** \(#function) *****")
+        
+        return "\(path.y).png"
+    }
+    
+    // path to X folder, starting from URLForTilecacheFolder
+    private func cacheXFolderNameForPath(path: MKTileOverlayPath) -> String {
+        /*============================================================*/
+        /*============================================================*/
+        /*============================================================*/
+        print ("***** \(#function) *****")
+        
+        return "\(path.contentScaleFactor)/\(path.z)/\(path.x)"
+    }
+    
+    // folder within app's Library/Caches to use for this particular overlay
+    private func URLForTilecacheFolder() -> URL {
+        /*============================================================*/
+        /*============================================================*/
+        /*============================================================*/
+        print ("***** \(#function) *****")
+        
+        let URLForAppCacheFolder : URL = try! FileManager.default.url(for: FileManager.SearchPathDirectory.cachesDirectory,
+                                                                      in: FileManager.SearchPathDomainMask.userDomainMask, appropriateFor: nil, create: true)
+        return URLForAppCacheFolder.appendingPathComponent(parentDirectory, isDirectory: true)
+    }
+    
+    private func URLForXFolder(path: MKTileOverlayPath) -> URL {
+        /*============================================================*/
+        /*============================================================*/
+        /*============================================================*/
+        print ("***** \(#function) *****")
+        
+        return URLForTilecacheFolder().appendingPathComponent(cacheXFolderNameForPath(path: path), isDirectory: true)
+    }
+}
